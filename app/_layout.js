a30343c0b3cf2964e7a800c62a2825d59d507340
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-05-25 20:09:11 +0300

    ADDED HOW TO HANDLE user input using <TextInput/> and useState Hook

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,5 @@
-import {Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
-export default function HomeLayout(){
-    return(
-        <Stack/>
-    )
+export default function HomeLayout() {
+  return <Stack />;
 }
--- app/createproduct.js
+++ app/createproduct.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react';
+import { TextInput } from 'react-native';
 
 const Createproduct = () => {
+  const [name, setName]= useState("");
+  const [price, setPrice]= useState("");
+  console.log(name, price);
   return (
     <div>
-      <p>Create Product</p>
+         <TextInput
+          placeholder="Name"
+          onChange={(event) => {
+            setName(event.nativeEvent.text);
+          }}
+        />
+           <TextInput
+          placeholder="Price"
+          onChange={(event) => {
+            setPrice(event.nativeEvent.text);
+          }}
+        />
     </div>
   )
 }
