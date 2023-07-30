9823ca9c36ec44d7ff2838c21b3b3155fe7b3e95
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-07-30 04:36:25 +0300

    ADDED <FlatList/> TUTORIAL

--- app/index.js
+++ app/index.js
@@ -1,5 +1,35 @@
-import { Text, View, TouchableOpacity, Touchable,StyleSheet } from "react-native";
+import { FlatList, Text, View, TouchableOpacity, Touchable,StyleSheet } from "react-native";
 import { useRouter, Link } from "expo-router";
+
+
+const Products = () =>{
+  return (
+    <View>
+      <FlatList
+      data={[
+        {key: "Product 1",
+         price: 29
+      },
+      {key: "Product 2",
+         price: 30
+      },
+      {key: "Product 3",
+         price: 35
+      },
+      {key: "Product 4",
+         price: 45
+      },
+      {key: "Product 5",
+         price: 50
+      },
+      
+      ]}
+      renderItem={({item})=> <Text style={styles.text}>{item.key}</Text>}
+      />
+
+    </View>
+  )
+}
 
 const Home = () => {
   const router = useRouter();
@@ -10,6 +40,7 @@ const Home = () => {
       <Link href="/createproduct">Create Product</Link>
 
       <Text onPress={() => router.push("/updateproduct")}>Update Product</Text>
+    <Products/>
     </View>
   );
 };
